47e029d59f950e6c59bbb8037fffd178b6467470
Mohsen Miran <108224707+Mohsen-Miran@users.noreply.github.com>
2023-02-05 17:03:09 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
+customuser = "mohsen"
+custompass = "Test$1234"
+customdb = "employeedb"
+custombucket = "addemployee19241364"
+customregion = "us-east-1"
